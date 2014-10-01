4aa2139795276fa1b80cac9f138ec8713f65ba4c
Ofri Raviv <ofri.raviv@gmail.com>
2014-10-02 02:31:42 +0300

    PEP8

--- setup_db.py
+++ setup_db.py
@@ -10,18 +10,22 @@ TEMP_DATA_DIR_NAME = 'opentrain_data'
 TAR_FILE_2013 = 'data/2013.tar.gz'
 TAR_FILE_2014 = 'data/2014-01Jan-to-10Aug.tar.gz'
 
+
 def extract_tar(tar_file, path):
-  if os.path.isdir(path):
-    shutil.rmtree(path)
-  os.mkdir(path)
-  print "Extracting data files to {}".format(path)
-  tar = tarfile.open(tar_file)
-  tar.extractall(path=path)
-  tar.close()
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
+    print "Extracting data files to {}".format(path)
+    tar = tarfile.open(tar_file)
+    tar.extractall(path=path)
+    tar.close()
+
 
 def fix_2014_file(full_filename, fixed_full_filename):
-  cmd = "cat " + full_filename + " | cut -c 2-39 | sed -r -e 's/^.{8}/& /' | awk -v OFS=\"\t\" '$1=$1' > " + fixed_full_filename
-  os.system(cmd)
+    cmd = "cat " + full_filename + " | cut -c 2-39 | " + \
+        "sed -r -e 's/^.{8}/& /' |" + \
+        "awk -v OFS=\"\t\" '$1=$1' > " + fixed_full_filename
+    os.system(cmd)
 
 start = time.time()
 
@@ -30,21 +34,25 @@ temp_data_dir = os.path.join(temp_dir, TEMP_DATA_DIR_NAME)
 src.manage.create_db()
 # load 2013 data:
 if True:
-  extract_tar(TAR_FILE_2013, temp_data_dir)
-  for filename in os.listdir(temp_data_dir):
-    full_filename = os.path.join(temp_data_dir, filename)
-    src.manage.add_opentrain_data(full_filename)
+    extract_tar(TAR_FILE_2013, temp_data_dir)
+    files = os.listdir(temp_data_dir)
+    for i, filename in enumerate(files):
+        print("processing file %d of %d in 2013 data" % (i + 1, len(files)))
+        full_filename = os.path.join(temp_data_dir, filename)
+        src.manage.add_opentrain_data(full_filename)
 
 # load 2014 data:
 if True:
-  extract_tar(TAR_FILE_2014, temp_data_dir)
-  filename = os.listdir(temp_data_dir)[0]  # there is just one file
-  full_filename = os.path.join(temp_data_dir, filename)
-  fixed_full_filename = os.path.join(temp_data_dir, '2014_fixed.txt')
-  fix_2014_file(full_filename, fixed_full_filename)
-  src.manage.add_opentrain_data(fixed_full_filename)
+    extract_tar(TAR_FILE_2014, temp_data_dir)
+    filename = os.listdir(temp_data_dir)[0]  # there is just one file
+    full_filename = os.path.join(temp_data_dir, filename)
+    fixed_full_filename = os.path.join(temp_data_dir, '2014_fixed.txt')
+    fix_2014_file(full_filename, fixed_full_filename)
+    print("processing file 1 of 1 in 2014 data")
+    src.manage.add_opentrain_data(fixed_full_filename)
 
 shutil.rmtree(temp_data_dir)
 
 elapsed = (time.time() - start)
-print "setup_db done. Elapsed time:", str(datetime.timedelta(seconds=round(elapsed)))
+print("setup_db done. Elapsed time:",
+      str(datetime.timedelta(seconds=round(elapsed))))
