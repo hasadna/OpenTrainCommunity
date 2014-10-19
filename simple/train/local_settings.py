9099b3ed4fcca1ae7fdff37287520b6489320730
Eran Keydar <ekeydar@gmail.com>
2014-10-19 22:08:55 +0300

    integrate with model

---
+++ simple/train/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES =  {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'train',                      
+        'USER': 'train',
+        'PASSWORD': 'train',
+        'HOST': 'localhost',
+        'PORT' : '5433'
+    }
+}
