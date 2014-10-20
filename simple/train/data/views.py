fc70ab72bbd5830ac4a5037ce80a8039c70d69fd
Eran Keydar <ekeydar@gmail.com>
2014-10-20 10:20:58 +0300

    wip

--- simple/train/data/views.py
+++ simple/train/data/views.py
@@ -1,7 +1,17 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
-def show_row_data(req):
-    return HttpResponse('not yet')
+def show_raw_data(req):
+    filename = req.GET['file']
+    lineno = int(req.GET['line'])
+    from_lineno = max(0,lineno-10)
+    to_lineno = (lineno+10)
+    ctx = dict()
+    cur_lineno = 1
+    lines = []
+    with open('parser/unzip_data/%s' % filename) as fh:
+        for line in fh:
+            if cur_lineno >= from_lineno and cur_lineno <= to_lineno:
+                lines.append({'lineno' : cur_lineno, 'line' : line.strip()})
+            lineno+=1
+    ctx['lines'] = lines
+    return render(req, 'data/raw_data.html', ctx)
 
