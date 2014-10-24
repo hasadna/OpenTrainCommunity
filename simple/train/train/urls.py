4c3d4e3eacfe8f4a68214159de1d1c846b5c562e
Eran Keydar <ekeydar@gmail.com>
2014-10-24 17:55:26 +0300

    wip

--- simple/train/train/urls.py
+++ simple/train/train/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'train.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^raw-data','data.views.show_raw_data')
+                       # Examples:
+                       # url(r'^$', 'train.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^raw-data', 'data.views.show_raw_data'),
+                       url(r'^results/from-to', 'data.views.show_results_from_to')
 )
 
 
