89f3dddcf604526cae1df6b2e0b5444697fd0836
Eran Keydar <ekeydar@gmail.com>
2014-10-25 11:38:14 +0300

    merge

--- simple/train/data/static/data/js/apps/FromTo.js
+++ simple/train/data/static/data/js/apps/FromTo.js
@@ -12,6 +12,8 @@ function($scope, MyHttp) {
         $scope.stops = data;
         $scope.input.fromStop = $scope.stops[0];
         $scope.input.toStop = $scope.stops[0];
+        $scope.input.fromHour = localStorage.fromHour || null;
+        $scope.input.toHour = localStorage.toHour || null;
         if (localStorage.fromStop || localStorage.toStop) {
             $scope.stops.forEach(function(stop) {
                 if (stop.stop_id == localStorage.fromStop) {
@@ -27,6 +29,8 @@ function($scope, MyHttp) {
         $scope.results = null;
         localStorage.setItem('fromStop',$scope.input.fromStop.stop_id);
         localStorage.setItem('toStop',$scope.input.toStop.stop_id);
+        localStorage.setItem('fromHour',$scope.input.fromHour);
+        localStorage.setItem('toHour',$scope.input.toHour);
         MyHttp.get('/api/routes/delays',
             {'from' : $scope.input.fromStop.stop_id,
             'to' : $scope.input.toStop.stop_id,
