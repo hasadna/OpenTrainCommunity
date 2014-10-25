30a6d410b6b7fa6e7bdcc6505c03c661163a8e46
Gil Zieder <gil.zieder@hp.com>
2014-10-25 17:44:00 +0300

    Added worst directions, doesn't work yet

--- simple/train/train/urls.py
+++ simple/train/train/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
                        url(r'^api/routes/delays', 'data.api.get_delay'),
                        url(r'^api/routes/worst_station', 'data.api.get_worst_station_in_route'),
                        url(r'^api/routes', 'data.api.get_route'),
+                       url(r'^api/stops/worst_direction','data.api.get_worst_direction'),
                        url(r'^api/stops','data.api.get_stops'),
                        url(r'^api/trips/(?P<trip_id>\w+)/','data.api.get_trip'),
                        url(r'^results/from-to', 'data.views.show_results_from_to')
