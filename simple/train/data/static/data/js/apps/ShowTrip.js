870f404fe0e433974d02d866ea780d63989c3489
Eran Keydar <ekeydar@gmail.com>
2014-11-03 22:06:21 +0200

    show trip

--- simple/train/data/static/data/js/apps/ShowTrip.js
+++ simple/train/data/static/data/js/apps/ShowTrip.js
@@ -1,9 +1,15 @@
 var app = angular.module('ShowTrip', ['my.filters','my.services','my.directives']);
 
-app.controller('ShowTripController', ['$scope', 'MyHttp',
-function($scope, MyHttp) {
+app.controller('ShowTripController', ['$scope', 'MyHttp','$timeout',
+function($scope, MyHttp,$timeout) {
     $scope.input = {
         tripId : null,
     };
+    $scope.go = function() {
+        $scope.tripId = null;
+        $timeout(function() {
+            $scope.tripId = $scope.input.tripId;
+        },100);
+    }
 }]);
 
