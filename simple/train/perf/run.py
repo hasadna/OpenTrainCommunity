707471fcddb6b36a123b2704817085ee906def69
Eran Keydar <ekeydar@gmail.com>
2014-12-29 21:19:40 +0200

    wip

--- simple/train/perf/run.py
+++ simple/train/perf/run.py
@@ -1,21 +1,39 @@
 import requests
 import json
 import time
+import sys
 
-def check(url,reffile):
+def check(url, reffile,ext):
+    if ext:
+        server = '104.131.88.144'
+        reffolder = 'ext'
+    else:
+        server = 'localhost:8000'
+        reffolder = 'local'
     t1 = time.time()
-    resp = requests.get('http://localhost:8000%s' % url)
+    final_url = 'http://%s%s' % (server,url)
+    resp = requests.get(final_url)
     t2 = time.time()
-    with open(reffile) as fh:
+    cur = resp.json()
+    with open('cur/%s' % reffile,'w') as fh:
+        json.dump(cur,fh)
+    with open('%s/%s' % (reffolder,reffile)) as fh:
         ref = json.load(fh)
-        assert ref == resp.json()
-    print 'Checked %s Took %.3f seconds' % (url,t2-t1)
-
+        result = ref == cur
+    print '%s %s Took %.3f seconds' % ('OK' if result else 'DIFF',final_url,t2-t1)
 
     
-check('/api/all-routes','all_routes.json')
-check('/api/route-info/?stop_ids=1600,1500,800,700,1300,1220,2100,2200,2300,2500,2800,2820,3100,3300,3400,3500,3600,3700,4600,4900,8600,300,400','route_info.json')
+def run(ext):
+    check('/api/all-routes','all_routes.json',ext=ext)
+    check('/api/route-info/?stop_ids=1600,1500,800,700,1300,1220,2100,2200,2300,2500,2800,2820,3100,3300,3400,3500,3600,3700,4600,4900,8600,300,400','route_info.json',ext=ext)
 
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ext',action='store_true')
+    ns = parser.parse_args()
+
+    run(ns.ext)
 
 
 
