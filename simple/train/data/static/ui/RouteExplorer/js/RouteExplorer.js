42dd9541e25bbb196a19677fb1c40732b2415dee
Avishay Lavie <avishay.lavie@gmail.com>
2015-01-05 21:38:23 +0200

    Route selection page WIP

--- simple/train/data/static/ui/RouteExplorer/js/RouteExplorer.js
+++ simple/train/data/static/ui/RouteExplorer/js/RouteExplorer.js
@@ -16,9 +16,39 @@ function($routeProvider) {
 app.controller('SelectRouteController', ['$scope', '$http',
 function($scope, $http) {
     $scope.stops = [];
+    var routes = [];
+
+    $scope.origin = null;
+    $scope.destination = null;
 
     $http.get('/api/stops')
         .success(function(data) {
             $scope.stops = data.map(function(s) { return { id: s.stop_id, name: s.stop_name, shortName: s.stop_short_name }; });
         });
+
+    $http.get('/api/all-routes')
+        .success(function(data) {
+            routes = data.map(function(r) { return {
+                stops: r.stops.map(function(s) { return s.stop_id; }),
+                count: r.count
+            }});
+        });
+
+    $scope.matchingRoutes = function(r) {
+        if (!$scope.origin || !$scope.destination)
+            return null;
+
+        return routes.filter(function(r) {
+            var originIndex = r.stops.indexOf($scope.origin.id);
+            var destinationIndex = r.stops.indexOf($scope.destination.id);
+
+            if (originIndex < 0 || destinationIndex < 0)
+                return false;
+
+            if (originIndex > destinationIndex)
+                return false;
+
+            return true;
+        });
+    }
 }])
