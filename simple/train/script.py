474b0f29185c8d518bb41d695f02ca44064c33c8
Eran Keydar <ekeydar@gmail.com>
2015-01-06 09:04:19 +0200

    script

---
+++ simple/train/script.py
@@ -0,0 +1,19 @@
+def main():
+    limit = 100
+    offset = 0
+    count = 0
+    total_trips = Trip.objects.count()
+    while True:
+        trips = Trip.objects.order_by('id')[offset:offset+1000]
+        trips = list(trips)
+        count += len(trips)
+        if not trips:
+            break
+            for trip in trips:
+                trip.sample_set.all().do_update(stop_ids=trip.stop_ids)
+                print 'Trips %s/%s done' % (count,total_trips)
+                offset+=100
+
+    print 'count = %s' % count
+
+
