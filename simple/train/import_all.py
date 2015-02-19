fecd1fbb11c1f1ae168efeb00372b6e94fbfcf73
Eran Keydar <ekeydar@gmail.com>
2015-02-19 22:44:24 +0200

    git ignore

--- simple/train/import_all.py
+++ simple/train/import_all.py
@@ -2,11 +2,24 @@
 import glob
 import os
 
+CSV_FILES = ['parser/output/01_2013.csv',
+             'parser/output/02_2013.csv',
+             'parser/output/03_2013.csv',
+             'parser/output/04_2013.csv',
+             'parser/output/05_2013.csv',
+             'parser/output/06_2013.csv',
+             'parser/output/07_2013.csv',
+             'parser/output/08_2013.csv',
+             'parser/output/09_2013.csv',
+             'parser/output/10_2013.csv',
+             'parser/output/11_2013.csv',
+             'parser/output/12_2013.csv',
+             'parser/output/2014.csv']
+
 def main():
-    csv_files = list(sorted(glob.glob('parser/output/*_2013.csv')))
-    for csv in csv_files:
+    for csv in CSV_FILES:
         res = os.system('python manage.py gensql %s' % csv)
-        assert res == 0
+        assert res == 0,'Failed in command'
 
 if __name__ == '__main__':
     main()
