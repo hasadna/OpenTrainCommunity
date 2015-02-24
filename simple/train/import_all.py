31e3f7e42d2271829cf868eefd98cf64b66b7401
Eran Keydar <ekeydar@gmail.com>
2015-02-24 09:42:44 +0200

    import all

--- simple/train/import_all.py
+++ simple/train/import_all.py
@@ -2,23 +2,26 @@
 import glob
 import os
 
-CSV_FILES = ['parser/output/01_2013.csv',
-             'parser/output/02_2013.csv',
-             'parser/output/03_2013.csv',
-             'parser/output/04_2013.csv',
-             'parser/output/05_2013.csv',
-             'parser/output/06_2013.csv',
-             'parser/output/07_2013.csv',
-             'parser/output/08_2013.csv',
-             'parser/output/09_2013.csv',
-             'parser/output/10_2013.csv',
-             'parser/output/11_2013.csv',
-             'parser/output/12_2013.csv',
-             'parser/output/2014.csv']
+CSV_DIR = '/home/opentrain/public_html/files/csv'
+
+CSV_FILES = ['01_2013.csv',
+             '02_2013.csv',
+             '03_2013.csv',
+             '04_2013.csv',
+             '05_2013.csv',
+             '06_2013.csv',
+             '07_2013.csv',
+             '08_2013.csv',
+             '09_2013.csv',
+             '10_2013.csv',
+             '11_2013.csv',
+             '12_2013.csv',
+             '2014.csv']
 
 def main():
     for csv in CSV_FILES:
-        res = os.system('python manage.py parsecsv %s' % csv)
+        fullcsv = os.path.join(CSV_DIR,csv)
+        res = os.system('python manage.py parsecsv %s' % fullcsv)
         assert res == 0,'Failed in command'
 
 if __name__ == '__main__':
