8c8bbd8bbb238f8b94ba764a12b073e8120c2b4b
Eran Keydar <ekeydar@gmail.com>
2015-03-09 20:58:34 +0200

    heb names

--- simple/train/data/heb_stop_names.py
+++ simple/train/data/heb_stop_names.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 HEB_NAMES = {
-    3700: ["תל אביב סבידור מרכז",'ת"א סבידור מרכז']
+    3700: ["תל אביב סבידור מרכז", 'ת"א סבידור מרכז']
 }
 
-for k,v in HEB_NAMES.iteritems():
-    assert isinstance(k,int),'for k = %s key must be integer' % k
-    assert isinstance(v,list),'for k = %s value must be list of string' % k
+for k, v in HEB_NAMES.iteritems():
+    assert isinstance(k, int), 'for k = %s key must be integer' % k
+    assert isinstance(v, list), 'for k = %s value must be list of string' % k
 
