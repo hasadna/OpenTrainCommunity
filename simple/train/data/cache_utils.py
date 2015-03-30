5789489b928c0ded676a535b3008c500b9e9e8f0
Eran Keydar <ekeydar@gmail.com>
2015-03-31 00:03:18 +0300

    no cache option

--- simple/train/data/cache_utils.py
+++ simple/train/data/cache_utils.py
@@ -18,14 +18,15 @@ def _build_key(req):
 
 def cacheit(func):
     def wrap(req, *args, **kwargs):
-        if CACHE_ENABLED:
+        use_cache = req.GET.get('no_cache',None) != '1' and CACHE_ENABLED
+        if use_cache:
             key = _build_key(req)
             cc = CLIENT.get(key)
             if cc:
-                print 'Return cached version of %s' % req.get_full_path()
+                #print 'Return cached version of %s' % req.get_full_path()
                 return HttpResponse(status=200, content=cc, content_type='application/json')
         result = func(req, *args, **kwargs)
-        if CACHE_ENABLED:
+        if use_cache:
             CLIENT.setex(key, TTL, result.content)
         return result
 
