80c13ec17ea48df32dcd6ac6b4536a5de5417fd0
Ofer Bartal <ofer.bartal@weizmann.ac.il>
2015-06-24 23:54:23 +0300

    Update website_page_ranking.py

--- simple/train/data/analysis/website_page_ranking.py
+++ simple/train/data/analysis/website_page_ranking.py
@@ -8,53 +8,71 @@ import data.api
 import json
 from data.models import Route
 
-filters = data.api.Filters(from_date=None,to_date=None)
-routes = Route.objects.all()
-routes = routes[0:1]
-res_list = []
-for route in routes[0:1]:
-  stops = route.stop_ids
-  res = data.api._get_route_info_full(route.id, filters) # Get all data from server 
-  res_list.append(res)
+DATA_FILE = '/home/opentrain/res.txt'
 
-all_route_ids=[route.id for route in routes]
-all_results = [all_route_ids, res_list]
-
-with open('/home/opentrain/res.txt', 'w') as outfile:
-  json.dump(all_results, outfile)
+def save_data():
+  filters = data.api.Filters(from_date=None,to_date=None)
+  routes = Route.objects.all()
+  # Remove this:
+  #routes = routes[0:1]
+  route_ids = []
+  route_info_list = []
+  for route in routes:
+    stops = route.stop_ids
+    print route.id
+    try:
+      route_info = data.api._get_route_info_full(route.id, filters) #  Get all data from server 
+      route_info_list.append(route_info)
+      route_ids.append(route.id)
+    except:
+        print 'cannot run', route.id
   
-
+  all_data = [route_ids, route_info_list]
   
-with open('/home/opentrain/res.txt', 'r') as outfile:
-  all_results = json.load(outfile)
+  with open(DATA_FILE, 'w') as outfile:
+    json.dump(all_data, outfile)
+  
+def analyse():
+  with open(DATA_FILE, 'r') as outfile:
+    all_data = json.load(outfile)
 
-routes = all_results[0]
-res_list = all_results[1]
-scores = []
-urls = []
+  route_ids = all_data[0]
+  route_info_list = all_data[1]
+  scores = []
+  urls = []
+  num_trips = []
 
-for route, res in zip(routes, res_list):
-  for sub_res in res:
-    if (sub_res['info']['num_trips'] == 0):
-      continue
-    alist = [float(stop['arrival_late_pct']) for stop in sub_res['stops']]
+  for route, route_info in zip(route_ids, route_info_list):
+    print route
+    for sub_route_info in route_info:
+      if (sub_route_info['info']['num_trips'] == 0):
+        continue
+      stop_values = [float(stop['arrival_late_pct']) for stop in sub_route_info['stops']]
 
-    # stats:
-    if len(alist)>0 :
-      stops_avg=float(sum(alist))/len(alist)
-      scores.append(stops_avg)
-      info=sub_res['info']
-      # otrain.org/ui/routes/#/route-details/13?time=9-12&day=3
-      url = 'otrain.org/ui/routes/#/route-details/{}?'.format(route)
-      if info['week_day'] != 'all':
-        url = url + 'day={}'.format(info['week_day'])
-      if info['hours'] != 'all':
-        url = url + 'time={}-{}'.format(info['hours'][0], info['hours'][1])
-      urls.append(url)
+      # stats:
+      if len(stop_values)>0 and sub_route_info['info']['num_trips'] > 100:
+        stops_avg = float(sum(stop_values))/len(stop_values)
+        scores.append(stops_avg)
+        num_trips.append(sub_route_info['info']['num_trips'])
+        info = sub_route_info['info']
+        # otrain.org/ui/routes/#/route-details/13?time=9-12&day=3
+        url = 'http://otrain.org/ui/routes/#/route-details/{}?'.format(route)
+        if info['week_day'] != 'all':
+          url = url + 'day={}'.format(info['week_day'])
+        if info['hours'] != 'all':
+          url = url + '&time={}-{}'.format(info['hours'][0], info['hours'][1])
+        urls.append(url)
 
-urls=[url for (score,url) in sorted(zip(scores,urls))]
-scores=sorted(scores)
+  reverse = True
+  urls=[url for (score,url) in sorted(zip(scores,urls), reverse=reverse)]
+  scores=sorted(scores, reverse=True)
 
-for i in xrange(10):
-  print urls[i]
-  print scores[i]
+  for i in xrange(100):
+    print urls[i]
+    print 'arrival late score', scores[i]
+    print 'num trips', num_trips[i]
+    print ''
+  print len(urls)
+
+
+analyse()
