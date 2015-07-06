b4b35a755e06f31f0c21f18573ab8ce1edecec64
Ofer Bartal <ofer.bartal@weizmann.ac.il>
2015-07-06 23:56:16 +0300

    More analysis, loading data from http

--- simple/train/data/analysis/website_page_ranking.py
+++ simple/train/data/analysis/website_page_ranking.py
@@ -1,31 +1,31 @@
 import os
 import sys
+import requests
 sys.path.append(os.getcwd())
 sys.path.append(os.path.dirname(os.getcwd()))
-os.environ['DJANGO_SETTINGS_MODULE']='train.settings'
+#os.environ['DJANGO_SETTINGS_MODULE']='train.settings'
 
-import data.api
+#import data.api
 import json
-from data.models import Route
+#from data.models import Route
 
-DATA_FILE = '/home/opentrain/res.txt'
+DATA_FILE = os.path.expanduser('~/tmp/res.txt')
+base_url = 'http://otrain.org/api/route-info-full?route_id={}'
 
 def save_data():
-  filters = data.api.Filters(from_date=None,to_date=None)
-  routes = Route.objects.all()
-  # Remove this:
-  #routes = routes[0:1]
   route_ids = []
   route_info_list = []
-  for route in routes:
-    stops = route.stop_ids
-    print route.id
+  for route_id in xrange(1,1000):
+    print route_id
     try:
-      route_info = data.api._get_route_info_full(route.id, filters) #  Get all data from server 
+      resp = requests.get(url=base_url.format(route_id))
+      if resp.status_code == 500:
+        break;      
+      route_info = json.loads(resp.text)      
       route_info_list.append(route_info)
-      route_ids.append(route.id)
+      route_ids.append(route_id)
     except:
-        print 'cannot run', route.id
+      print 'cannot run', route_id
   
   all_data = [route_ids, route_info_list]
   
@@ -48,9 +48,10 @@ def analyse():
       if (sub_route_info['info']['num_trips'] == 0):
         continue
       stop_values = [float(stop['arrival_late_pct']) for stop in sub_route_info['stops']]
+      stop_values.extend([float(stop['departure_early_pct']) for stop in sub_route_info['stops']])
 
       # stats:
-      if len(stop_values)>0 and sub_route_info['info']['num_trips'] > 100:
+      if len(stop_values)>0 and sub_route_info['info']['num_trips'] > 500:
         stops_avg = float(sum(stop_values))/len(stop_values)
         scores.append(stops_avg)
         num_trips.append(sub_route_info['info']['num_trips'])
@@ -67,12 +68,65 @@ def analyse():
   urls=[url for (score,url) in sorted(zip(scores,urls), reverse=reverse)]
   scores=sorted(scores, reverse=True)
 
-  for i in xrange(100):
+
+  print '*** High error to low ***'
+  for i in xrange(25):
     print urls[i]
     print 'arrival late score', scores[i]
     print 'num trips', num_trips[i]
     print ''
+
+  print '*** Low error to high **********************************************************************'
+  for i in xrange(25):
+    ind = len(urls) - i - 1
+    print urls[ind]
+    print 'arrival late score', scores[ind]
+    print 'num trips', num_trips[ind]
+    print ''
   print len(urls)
 
 
-analyse()
+def analyse2():
+  import matplotlib.pyplot as plt
+  import numpy as np
+  
+  with open(DATA_FILE, 'r') as outfile:
+    all_data = json.load(outfile)
+
+  route_ids = all_data[0]
+  route_info_list = all_data[1]
+  x = []
+  y = []
+
+  for route, route_info in zip(route_ids, route_info_list):
+    print route
+    for sub_route_info in route_info:
+      if (sub_route_info['info']['num_trips'] < 100):
+        continue
+      stop_values = [float(stop['arrival_late_pct']) for stop in sub_route_info['stops']]
+      for idx, val in enumerate(stop_values):
+        x.append(idx/float(len(stop_values)))
+        y.append(val)
+
+  print x[0:100]
+  print y[0:100]
+  
+  k = 12
+  means = []
+  for i in xrange(k):
+    means.append([])
+  
+  for xa, ya in zip(x,y):
+    if ya:
+      means[int(xa*k)].append(ya)
+  
+  mean_vals = [np.mean(means[i]) for i in xrange(k)]  
+  plt.plot([int(float(i)/(k-1)*100) for i in xrange(k)], mean_vals)
+  plt.xlabel('Route percentage')
+  plt.ylabel('Percent late')
+  plt.title('Route lateness progression')
+  plt.show()
+
+res = analyse2()
+#save_data()
+#analyse()
