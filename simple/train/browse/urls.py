56e6bb537ffe034d0789f9691566e43c5c63e29e
Eran Keydar <ekeydar@gmail.com>
2015-07-12 17:35:31 +0300

    user

--- simple/train/browse/urls.py
+++ simple/train/browse/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth import views as auth_views
 import views
 
 urlpatterns = patterns('',
@@ -9,5 +10,7 @@ urlpatterns = patterns('',
                        url(r'routes/(?P<route_id>\d+)/skip-stops/?$',views.skip_unskip_stops,{'skip':False}),
                        url(r'services/(?P<service_id>\d+)/?$',views.browse_service),
                        url(r'trips/(?P<trip_id>\w+)/?$',views.browse_trip),
+                       #url(r'login/?$',views.login),
+                       #url(r'logout/?$',views.logout),
                        url(r'^raw-data', views.show_raw_data))
 
