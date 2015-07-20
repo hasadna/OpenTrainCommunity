bbae70d4e25b5d0700fc57a4a1f2a221938e34e4
Eran Keydar <ekeydar@gmail.com>
2015-07-20 19:51:17 +0300

    requirements

--- simple/train/csvparser/management/commands/parsecsv.py
+++ simple/train/csvparser/management/commands/parsecsv.py
@@ -2,11 +2,11 @@ from django.core.management.base import BaseCommand, CommandError
 import data.utils
 
 class Command(BaseCommand):
-    args = ''
-    help = 'parse csv'
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('files', nargs='*')
 
     def handle(self, *args, **options):
-        if not args:
-            raise CommandError('must give filename')
-        for filename in args:
+        for filename in options['files']:
             data.utils.import_current_csv(filename)
