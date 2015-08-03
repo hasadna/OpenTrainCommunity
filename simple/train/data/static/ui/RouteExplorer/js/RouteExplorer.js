01013fbd4c20890bc2a4ed438d205617aa0b3c65
Eran Keydar <ekeydar@gmail.com>
2015-08-03 21:16:24 +0300

    fix fb link to new scheme

--- simple/train/data/static/ui/RouteExplorer/js/RouteExplorer.js
+++ simple/train/data/static/ui/RouteExplorer/js/RouteExplorer.js
@@ -56,7 +56,7 @@ function($routeProvider) {
 app.controller('AppController', ['$scope', '$location',
 function($scope, $location) {
     $scope.share = function(prefix) {
-        var url = prefix + encodeURIComponent('http://otrain.org/ui/routes/#' + $location.url());
+        var url = prefix + encodeURIComponent('http://otrain.org/#' + $location.url());
         window.open(url, 'sharePopup', 'width=600,height=550,top=100,left=100,location=no,scrollbar=no,status=no,menubar=no');
     };
 
