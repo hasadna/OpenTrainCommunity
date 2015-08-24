5648310d6f6f85337b1f226a510a88a4c66f13cf
Eran Keydar <ekeydar@gmail.com>
2015-08-24 21:00:57 +0300

    xl wip

--- simple/train/xlparser/utils.py
+++ simple/train/xlparser/utils.py
@@ -1,46 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 import xlrd
 import unicodecsv
 import os
+import datetime
+from django.utils import timezone
+
+HEADER_MAPPING = dict()
+HEADER_MAPPING['תאריך נסיעת רכבת'] = 'train_date'
+HEADER_MAPPING['מספר רכבת'] = 'train_num'
+HEADER_MAPPING['רכבת מתוכננת/לא מתוכננת'] = 'is_planned'
+HEADER_MAPPING['מספר תחנה'] = 'stop_id'
+HEADER_MAPPING['תאור תחנה'] = 'stop_name'
+HEADER_MAPPING['מספר סידורי של התחנה'] = 'index'
+HEADER_MAPPING['תאור קו נוסעים'] = 'route_nme'
+HEADER_MAPPING['אופי התחנה'] = 'stop_kind'
+HEADER_MAPPING['האם תחנת עצירה מתוכננת'] = 'is_planned'
+HEADER_MAPPING['האם תחנת עצירה בפועל'] = 'is_stopped'
+HEADER_MAPPING['תאריך ושעת יציאה מהתחנה מתוכנן'] = 'exp_departure'
+HEADER_MAPPING['תאריך ושעת יציאה מהתחנה בפועל'] = 'actual_departure'
+HEADER_MAPPING['תאריך ושעת הגעה לתחנה מתוכנן'] = 'exp_arrival'
+HEADER_MAPPING['תאריך ושעת הגעה לתחנה בפועל'] = 'actual_arrival'
+
 
 def parse_xl(xlname, csvname=None):
-    if csvname is None:
-        csvname = os.path.splitext(xlname)[0] + '.csv'
-    with open(csvname,'w') as fh:
-        wr = unicodecsv.writer(fh, quoting=unicodecsv.QUOTE_ALL)
-        wb = xlrd.open_workbook(xlname)
-        sheet = wb.sheet_by_index(0)
-        header = None
-        good = 0
-        bad = 0
-        for rownum in xrange(sheet.nrows):
-            values = sheet.row_values(rownum)
-            clean_values = clean_row(rownum, values)
-            if clean_values:
-                if not header:
-                    header = clean_values
-                else:
-                    assert len(header) == len(clean_values)
-                    xl_row_to_csv(header, clean_values)
-                    good+=1
-                    if good == 1:
-                        return
+    wb = xlrd.open_workbook(xlname)
+    sheet = wb.sheet_by_index(0)
+    heb_header = [sheet.cell_value(3, colx) for colx in xrange(1, sheet.ncols)]
+    header = [HEADER_MAPPING[h] for h in heb_header]
+
+    for rowx in xrange(4, sheet.nrows):
+        row = []
+        for colx in xrange(1, sheet.ncols):
+            cell_value = sheet.cell_value(rowx, colx)
+            cell_type = sheet.cell_type(rowx, colx)
+            if cell_type == xlrd.XL_CELL_DATE:
+                dt_tuple = xlrd.xldate_as_tuple(cell_value, wb.datemode)
+                dt = datetime.datetime(*dt_tuple)
+                dt = timezone.get_default_timezone().localize(dt)
+                row.append(dt)
             else:
-                bad+=1
-
-    print 'Bad rows: %s' % bad
-    print 'Header row: %s' % 1 if header is not None else 0
-    print 'Good rows: %s' % good
-    print 'Wrote csv to {0}'.format(csvname)
+                row.append(cell_value)
+        xl_row_to_csv(dict(zip(header,row)))
+        return
 
 
-def clean_row(row_num, values):
-    empties = len([v for v in values[1:] if v is None or v == ''])
-    if  len(values) - empties >= 4:
-        return values[1:]
-    return None
+CSV_HEADER = ['train_num',
+                'start_date',
+                'trip_name',
+                'index',
+                'stop_id',
+                'stop_name',
+                'is_real_stop',
+                'valid',
+                'is_first',
+                'is_last',
+                'actual_arrival',
+                'exp_arrival',
+                'delay_arrival',
+                'actual_departure',
+                'exp_departure',
+                'delay_departure',
+                'data_file',
+                'data_file_line',
+                'data_file_link'
+                ]
 
 
-def xl_row_to_csv(header, values):
-    for h,v in zip(header, values):
-        print '%s: %s' % (h[::-1],v)
+def xl_row_to_csv(row_dict):
+    print row_dict
+
+
+    output_dict = dict()
+    for n in CSV_HEADER:
+        output_dict[n] = None
 
