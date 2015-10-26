7f366d3fcaa50e873b1aaa83dddeadd0d308a403
Eran Keydar <ekeydar@gmail.com>
2015-10-26 19:44:14 +0200

    dump

--- simple/train/data/models.py
+++ simple/train/data/models.py
@@ -258,7 +258,7 @@ class Trip(models.Model):
         return reverse('browse:trip',kwargs=dict(pk=self.id))
 
     class Meta:
-        ordering = ['route_id','start_date','x_hour_local']
+        ordering = ['route_id','start_date','x_hour_local','id']
 
 
 class Route(models.Model):
