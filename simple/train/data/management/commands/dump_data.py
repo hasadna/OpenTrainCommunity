02ea97c814e1641af7a39e56ae1b73467cca5fea
Eran Keydar <ekeydar@gmail.com>
2015-10-26 20:18:05 +0200

    dump

--- simple/train/data/management/commands/dump_data.py
+++ simple/train/data/management/commands/dump_data.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 import json
 import os
-from data.models import Route, Trip
+from data.models import Route, Trip, Sample
 
 class Command(BaseCommand):
     args = ''
@@ -14,6 +14,7 @@ class Command(BaseCommand):
         self.dirname = options['dir']
         self.dump_routes()
         self.dump_trips()
+        self.dump_services()
 
     def dump_routes(self):
         routes = []
@@ -24,6 +25,20 @@ class Command(BaseCommand):
             })
         self.dump(routes, 'routes')
 
+    def dump_services(self):
+        samples = []
+        for sample in Sample.objects.filter(valid=True):
+            samples.append({
+                'id': sample.id,
+                'trip': sample.trip.id,
+                #'service': sample.trip.service.id,
+                'stop_id': sample.stop_id,
+                'index': sample.index,
+                'exp_departure': sample.exp_departure.isoformat() if sample.exp_departure else None
+            })
+
+        self.dump(samples,'samples')
+
     def dump_trips(self):
         trips = []
         for trip in Trip.objects.all():
@@ -32,7 +47,7 @@ class Command(BaseCommand):
                 'route_id': trip.route_id,
                 'start_data': trip.start_date.isoformat(),
                 'x_hour_local': trip.x_hour_local,
-                #'x_week_day_local': trip.x_week_day_local,
+                'x_week_day_local': trip.x_week_day_local,
             })
         self.dump(trips, 'trips')
 
