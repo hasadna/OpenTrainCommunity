a922ffaf9b54d93f4b9394c593af9160f9d6aafe
Eran Keydar <ekeydar@gmail.com>
2015-10-26 19:13:49 +0200

    dump

--- simple/train/data/management/commands/dump_data.py
+++ simple/train/data/management/commands/dump_data.py
@@ -1,24 +1,29 @@
 from django.core.management.base import BaseCommand, CommandError
 import json
+import os
 from data.models import Route
 
 class Command(BaseCommand):
     args = ''
-    help = 'dump routes'
+    help = 'dump data'
 
     def add_arguments(self, parser):
-        parser.add_argument('--out',mandatory=True,type=str)
+        parser.add_argument('--dir',required=True,type=str)
 
     def handle(self, *args, **options):
         result = []
+        dirname = options['dir']
         for route in Route.objects.all():
             result.append({
                 'id': route.id,
                 'stop_ids': route.stop_ids,
             })
-        with open(options['out'],'w') as fh:
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+        fname = os.path.join(dirname,'routes.json')
+        with open(fname,'w') as fh:
             json.dump(result, fh,indent=4,sort_keys=True)
-        print('Wrote routes to {0}'.format(options['out']))
+        print('Wrote routes to {0}'.format(fname))
 
-        
+
 
