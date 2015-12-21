f2451799bb0c0d3b55a81dee1364ef39fd35d287
Eran Keydar <ekeydar@gmail.com>
2015-12-21 18:19:58 +0200

    clean_all.py

--- simple/train/clean_all.py
+++ simple/train/clean_all.py
@@ -4,6 +4,7 @@ import subprocess
 import os
 import platform
 import argparse
+import tempfile
 
 
 def main():
@@ -11,10 +12,18 @@ def main():
     parser.add_argument('--no-create', action='store_true')
     parser.add_argument('--restore')
     parser.add_argument('--no-postgres-user', action='store_true')
+    parser.add_argument('--dry',action='store_true')
     g = parser.add_mutually_exclusive_group()
     g.add_argument('--sudo', action='store_true')
     g.add_argument('--nosudo', action='store_true')
     options = parser.parse_args()
+
+    def run_cmd(cmd):
+        if not options.dry:
+            subprocess.call(cmd, shell=True)
+        else:
+            print(cmd)
+
 
     process = subprocess.Popen(['python', 'manage.py', 'print_db'], stdout=subprocess.PIPE)
     out, err = process.communicate()
@@ -40,15 +49,26 @@ def main():
         if use_sudo:
             postgres_cmd = "sudo -u postgres psql"
         else:
-            postgres_cmd = "psql -U postgres"  # this might also work on Linux but I can't check
+            if options.no_postgres_user:
+                postgres_cmd = "psql"
+            else:
+                postgres_cmd = "psql -U postgres"
 
-        subprocess.call('python manage.py sqlcreate -D --router=default | ' + postgres_cmd, shell=True)
+        run_cmd('python manage.py sqlcreate -D --router=default | ' + postgres_cmd)
 
     if not (options.no_create or options.restore):
-        subprocess.call('python manage.py migrate', shell=True)
+        run_cmd('python manage.py migrate')
     if options.restore:
-        subprocess.call('{0} --set ON_ERROR_STOP=on traindata < {1}'.format(postgres_cmd, options.restore), shell=True)
-        subprocess.call('python manage.py clear_cache', shell=True)
+        if options.no_postgres_user:
+            tmp1 = os.path.join(tempfile.gettempdir(),os.path.basename(options.restore))
+            with open(options.restore) as fr, open(tmp1,'w') as fh:
+                for line in fr:
+                    if 'postgres' not in line:
+                        fh.write(line)
+            options.restore = tmp1
+
+        run_cmd('{0} --set ON_ERROR_STOP=on traindata < {1}'.format(postgres_cmd, options.restore))
+        run_cmd('python manage.py clear_cache')
     return 0
 
 
