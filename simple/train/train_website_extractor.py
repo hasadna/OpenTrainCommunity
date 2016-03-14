46fb5b16edc77405c50f29d08597e450e10b4349
amitzini <amittzini@gmail.com>
2016-03-14 21:42:35 +0200

    Fixed the code by Ofer's comments

--- simple/train/train_website_extractor.py
+++ simple/train/train_website_extractor.py
@@ -1,32 +1,43 @@
 from lxml import html
 import requests
 
-def getdepturetime(orgid,destid,day,month,year,hour):
-    ##
-    ##orgid- the station id which the trains depture
-    ##destid- the station id which the train needs to arrive
-    ##day-the day of the month of the ride
-    ##moth-the month of the ride(number)
-    ##year-the year of the ride(number)
-    ##hour-the hour of the ride(number)
-    allhours=[]
-    #the list that will contain all the depture hours and will get returned
+def get_depture_time(orgid, destid, day, month, year, hour):
 
-    page = requests.get(('https://www.rail.co.il/HE/DrivePlan/Pages/DrivePlan.aspx?DrivePlanPage=true&'
+    """ Extract depture times from the official Israel Railways' site
+    Args:
+        orgid: Station's id which the train depture from
+        destid: Station's id which the train goes
+        day: the day of the month when the ride takes place(integer from 1 to 31 according to the month)
+        month: the month when the ride takes place (integer from 1 to 12)
+        year: the year when the ride takes place(integer in the scheme of YYYY)
+        hour: the hour when the ride takes place (integer from 0 to 23)
+
+    Returns:
+        A list of the departure hours according to the arguments
+
+        example:
+        ['0:52', '1:52', '2:52', '3:52', '4:35']
+
+        If the list is returned empty there's no depture times whith the chosen arguments
+
+    """
+    allhours = []
+    #the list that will contain all the depture hours and will get returned
+    urlstr = ('https://www.rail.co.il/HE/DrivePlan/Pages/DrivePlan.aspx?DrivePlanPage=true&'
                          'OriginStationId={}&'
                          'DestStationId={}&'
                          'HoursDeparture={}&'
                          'MinutesDeparture=0&'
                          'GoingTrainCln={}-{}-{}&'
-                         'IsFullURL=true').format(orgid,destid,hour,year,month,day))
-    tree=html.fromstring(page.content)
-    i=0
-    hr=tree.xpath('//tr[@id="{}"]/td[position()=2]/text()'.format(i))
-    ##loops thats exports the depture times until theres no depture times in the html file
-    while(len(hr)!=0):
-        allhours.append(hr[0])
-        i=i+1
-        hr=tree.xpath('//tr[@id="{}"]/td[position()=2]/text()'.format(i))
+                         'IsFullURL=true').format(orgid,destid,hour,year,month,day)
 
+    page = requests.get(urlstr)
+    html_tree = html.fromstring(page.content)
+    i = 0
+    # loops thats exports the depture times until theres no depture times in the html file
+    while (len(hours) != 0 or i == 0):
+        hours = html_tree.xpath('//tr[@id="{}"]/td[position()=2]/text()'.format(i))
+        allhours.append(hours[0])
+        i = i + 1
 
     return allhours
