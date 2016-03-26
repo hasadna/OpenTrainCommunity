ae109a9c0367b2b44077d65882378d31fb27a5df
Eran Keydar <ekeydar@gmail.com>
2016-03-26 17:01:42 +0300

    wip

--- train2/train2/api_urls.py
+++ train2/train2/api_urls.py
@@ -1,26 +1,26 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-import data.drf
+import data.api
 
 router = routers.SimpleRouter()
 
-router.register('stops', data.drf.StopViewSet)
-router.register('stats', data.drf.StatViewSet, base_name="stats")
-router.register('routes', data.drf.RoutesViewSet)
+router.register('stops', data.api.StopViewSet)
+router.register('stats', data.api.StatViewSet, base_name="stats")
+router.register('routes', data.api.RoutesViewSet)
 router.register('routes/(?P<route_id>\d+)/trips',
-                data.drf.RouteTripsViewSet,
+                data.api.RouteTripsViewSet,
                 base_name='route-trips')
 
 # router.register('routes/(?P<route_id>\d+)/services',
-#                 data.drf.RouteServicesViewSet,
+#                 data.api.RouteServicesViewSet,
 #                 base_name='route-services')
 
 # router.register('routes/(?P<route_id>\d+)/services/(?P<service_id>\d+)/trips',
-#                 data.drf.RouteServicesViewSet,
+#                 data.api.RouteServicesViewSet,
 #                 base_name='service-trips')
 
 # router.register('routes/(?P<route_id>\d+)/services/(?P<service_id>\d+)/trips/(?P<trip_id>[^/.]+)/samples',
-#                 data.drf.TripSamplesViewSet,
+#                 data.api.TripSamplesViewSet,
 #                 base_name='trip-samples')
 #
 
