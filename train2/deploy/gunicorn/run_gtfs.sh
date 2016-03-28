fab65e5daebde8d407d2636ddbd1590da65d9e98
Eran Keydar <ekeydar@gmail.com>
2016-03-28 20:23:25 +0300

    deploy

--- train2/deploy/gunicorn/run_gtfs.sh
+++ train2/deploy/gunicorn/run_gtfs.sh
@@ -3,6 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source /home/opentrain/.virtualenvs/opentrain2/bin/activate
 cd $DIR
 # make sure this is the same port as in nginx conf file
-exec gunicorn -p /home/opentrain/train2.pid -b 127.0.0.1:9002 -w 2 -t 90 opentrain2.wsgi:application
+exec gunicorn -p /home/opentrain/gtfs.pid -b 127.0.0.1:9002 -w 2 -t 90 opentrain2.wsgi:application
 
 
