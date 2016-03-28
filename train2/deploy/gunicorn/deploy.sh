45b68b4b4ce708ec3dc82118da3d62a47cfa24e6
Eran Keydar <ekeydar@gmail.com>
2016-03-28 20:33:53 +0300

    deploy

---
+++ train2/deploy/gunicorn/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DEPLOY=/home/opentrain/work/OpenTrainCommunity/train2/deploy/
+WORK=/home/opentrain/work
+
+cp $DEPLOY/gunicorn/run_train2.sh $WORK/OpenTrainCommunity/train2/
+cp $DEPLOY/gunicorn/run_opentrain.sh $WORK/OpenTrainCommunity/simple/train/
+cp $DEPLOY/gunicorn/run_gtfs.sh $WORK/OpenTrain2/server/
+
+sudo cp $DEPLOY/nginx/opentrain.conf /etc/nginx/sites-available/opentrain.conf
+
+sudo cp $DEPLOY/supervisor/opentrain.conf /etc/supervisor/conf.d/opentrain.conf
+
+sudo service supervisor restart
+sudo service nginx reload
+sudo service nginx restart
+
