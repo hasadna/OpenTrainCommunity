2bb0c9a6e8f8278735ef060f95e7b68d3687cea7
Eran Keydar <ekeydar@gmail.com>
2016-03-28 20:00:17 +0300

    deploy

---
+++ train2/deploy/gunicorn/run_gtfs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source /home/opentrain/.virtualenvs/opentrain2/bin/activate
+cd $DIR
+# make sure this is the same port as in nginx conf file
+exec gunicorn -p /home/opentrain/train2.pid -b 127.0.0.1:9002 -w 2 -t 90 opentrain2.wsgi:application
+
+
---
+++ train2/deploy/gunicorn/run_opentrain.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source /home/opentrain/.virtualenvs/opentrain/bin/activate
+cd $DIR
+# make sure this is the same port as in nginx conf file
+exec gunicorn -p /home/opentrain/opentrain.pid -b 127.0.0.1:9001 -w 3 -t 90 opentrain.wsgi:application
+
