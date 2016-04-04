9f0a7741440935452dc8b68974a382cb19d5e5ee
Eran Keydar <ekeydar@gmail.com>
2016-04-04 19:09:19 +0300

    dump

--- train2/dump_db.sh
+++ train2/dump_db.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-sudo -u postgres pg_dump train2 > /home/opentrain/public_html/files/dumps/db_$(date +'%Y_%m_%d_%H_%M_%S').sql
-gzip --keep /home/opentrain/public_html/files/dumps/db_$(date +'%Y_%m_%d_%H_%M_%S').sql
+out=/home/opentrain/public_html/files/dumps/db_$(date +'%Y_%m_%d_%H_%M_%S').sql
+sudo -u postgres pg_dump train2 > ${out}
+gzip --keep ${out}.sql
 
