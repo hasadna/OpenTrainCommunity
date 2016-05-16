8ab2285a48622f01a188aeec6a045ee6331a1386
Eran Keydar <ekeydar@gmail.com>
2016-05-16 21:12:11 +0300

    explore

--- train2/ui/static/ui/RouteExplorer/js/controllers/Explore.js
+++ train2/ui/static/ui/RouteExplorer/js/controllers/Explore.js
@@ -1,5 +1,8 @@
 angular.module('RouteExplorer').controller('ExploreController',
 ['$scope', '$route', '$http', '$location', 'LocationBinder', 'Layout', 'Locale', 'TimeParser',
 function($scope, $route, $http, $location, LocationBinder, Layout, Locale, TimeParser) {
+    $scope.days = Locale.days;
+    $scope.input = {
 
+    }
 }]);
