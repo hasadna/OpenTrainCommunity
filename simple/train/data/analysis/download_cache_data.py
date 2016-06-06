e05a15db0640bab7dea375ac95b03f5c127641c6
Ofer Bartal <greenofer@gmail.com>
2016-06-06 08:21:55 +0300

    Fixing pylint errors

--- simple/train/data/analysis/download_cache_data.py
+++ simple/train/data/analysis/download_cache_data.py
@@ -20,11 +20,11 @@ import datetime
 
 SERVER_DOMAIN = 'otrain.org'
 LOCAL_DOMAIN = '127.0.0.1:8000'
-DOMAIN = LOCAL_DOMAIN
+DOMAIN = SERVER_DOMAIN
 
 API_ALL_ROUTES_URL = 'http://{}/api/v1/routes/all/'.format(DOMAIN)
 API_BASE_URL = 'http://{}/api/v1/stats/route-info-full/'.format(DOMAIN)
-API_PARAMS = '?from_date={}&route_id={}&to_date={}' # '?route_id={}&from_date={}&to_date={}'
+API_PARAMS = '?from_date={}&route_id={}&to_date={}'
 
 
 TIMEZONE_OFFSET = -2*60*60  # NB: this is not perfect due to daylight savings time, only used if USE_MICROSECONDS is True
@@ -40,53 +40,52 @@ TIME_PERIODS = [[2016, [1, 2, 3]],
                 [2015, range(1, 13)]]
 
 if not os.path.exists('cache'):
-   print('Creating cache directory...')
-   os.makedirs('cache')
+  print('Creating cache directory...')
+  os.makedirs('cache')
 
 response = requests.get(API_ALL_ROUTES_URL)
 response_json = json.loads(response.text)
 
 if not ROUTES:
-   routes_all = []
-   for r in response_json:
-      routes_all.append(r['id'])
+  routes_all = []
+  for r in response_json:
+    routes_all.append(r['id'])
 else:
-   routes_all = ROUTES
+  routes_all = ROUTES
 
 for route_id in routes_all:
-   if True or not os.path.isfile('cache/cache_%d.json' % route_id):
-      data_for_this_route = []
-      for year, months in TIME_PERIODS:
-         for month in months:
-            days = calendar.monthrange(year, month)[1]
-            print('Route %d Year %d Month %d Days %d' % (route_id, year, month, days))
-            if USE_MICROSECONDS:
-               from_date = datetime.datetime(year, month, 1, 0, 0, 0, 0)
-               from_date = (calendar.timegm(from_date.timetuple())+TIMEZONE_OFFSET)
-               to_date = datetime.datetime(year, month, days, 0, 0, 0, 0)
-               to_date = (calendar.timegm(to_date.timetuple())+TIMEZONE_OFFSET)
-               url = API_BASE_URL + API_PARAMS.format(from_date * 1000, route_id, to_date * 1000)
-            else:
-               from_date = "%d/%d/%d" % (1, month, year)
-               to_date = "%d/%d/%d" % (days, month, year)
-               url = API_BASE_URL + API_PARAMS.format(from_date, route_id, to_date)
+  if True or not os.path.isfile('cache/cache_%d.json' % route_id):
+    data_for_this_route = []
+    for year, months in TIME_PERIODS:
+      for month in months:
+        days = calendar.monthrange(year, month)[1]
+        print('Route %d Year %d Month %d Days %d' % (route_id, year, month, days))
+        if USE_MICROSECONDS:
+          from_date = datetime.datetime(year, month, 1, 0, 0, 0, 0)
+          from_date = (calendar.timegm(from_date.timetuple())+TIMEZONE_OFFSET)
+          to_date = datetime.datetime(year, month, days, 0, 0, 0, 0)
+          to_date = (calendar.timegm(to_date.timetuple())+TIMEZONE_OFFSET)
+          url = API_BASE_URL + API_PARAMS.format(from_date * 1000, route_id, to_date * 1000)
+        else:
+          from_date = "%d/%d/%d" % (1, month, year)
+          to_date = "%d/%d/%d" % (days, month, year)
+          url = API_BASE_URL + API_PARAMS.format(from_date, route_id, to_date)
 
-            print(url)
-            try:
-               response = requests.get(url=url)
-               if (200 != response.status_code):
-                  print('ERROR: Status code not 200 OK on route %d' % route_id)
-               else:
-                  route_info = json.loads(response.text)
-                  data_for_this_route.append([[year, month], route_info])
-            except KeyboardInterrupt:
-               print('Exiting')
-               exit(1)
-            except:
-               print('ERROR: Exception receiving response on route %d' % route_id)
-
-      with open('cache/cache_%d.json'%route_id, 'w') as output_file:
-         json.dump(data_for_this_route, output_file)
-   else:
-      print('Route %d skipped--cache file already exists' % route_id)
+        print(url)
+        try:
+          response = requests.get(url=url)
+          if response.status_code != 200:
+            print('ERROR: Status code not 200 OK on route %d' % route_id)
+          else:
+            route_info = json.loads(response.text)
+            data_for_this_route.append([[year, month], route_info])
+        except KeyboardInterrupt:
+          print('Exiting')
+          exit(1)
+        except:
+          print('ERROR: Exception receiving response on route %d' % route_id)
+    with open('cache/cache_%d.json'%route_id, 'w') as output_file:
+      json.dump(data_for_this_route, output_file)
+  else:
+    print('Route %d skipped--cache file already exists' % route_id)
 
--- simple/train/data/analysis/opentrain_analysis.py
+++ simple/train/data/analysis/opentrain_analysis.py
@@ -1,3 +1,4 @@
+"""Functions to help in analysis of website data using ipython notebook"""
 import json
 import os
 import requests
@@ -18,21 +19,23 @@ HOURS = ['all',
          ]
 
 def get_stops():
+  """Return a list stops from the website API"""
   all_stops = json.loads(requests.get('http://otrain.org/api/v1/stops').text)
-  stop_lut = {}
+  result = {}
   for stop_info in all_stops:
-    stop_lut.update({stop_info['stop_id'] : [stop_info['heb_stop_names'][0], stop_info['stop_name']]})
-  return stop_lut
+    result.update({stop_info['stop_id']: [stop_info['heb_stop_names'][0], stop_info['stop_name']]})
+  return result
 
 
 def get_routes_above_threshold(num_trips_threshold):
+  """Return a list of route ids with trip count above @num_trips_threshold"""
   result = []
   for route_id in xrange(0, 10000):
     passed_filter = False
     if os.path.isfile("%scache_%s.json" % (CACHE_PATH, route_id)):
       with open("%scache_%s.json" % (CACHE_PATH, route_id), 'r') as input_file:
         route_data = json.load(input_file)
-        for time, data in route_data:
+        for _, data in route_data:
           for entry in data:
             # Look at either days='all' or hours='all' and limit to minimal num_trip count
             if ((entry['info']['week_day'] == 'all' or entry['info']['hours'] == 'all') and
@@ -44,7 +47,8 @@ def get_routes_above_threshold(num_trips_threshold):
 
 
 def get_route_by_id(route_id, num_trips_threshold):
-  df = pd.DataFrame()
+  """Return route information by @route_id, filtered by @num_trips_threshold"""
+  result = pd.DataFrame()
   with open("%scache_%s.json" % (CACHE_PATH, route_id), 'r') as input_file:
     route_data = json.load(input_file)
     for time, data in route_data:
@@ -65,6 +69,6 @@ def get_route_by_id(route_id, num_trips_threshold):
             stops.update({'url':URL_GUI % (int(time[0]), int(time[1]),
                                            int(route_id), url_day, url_hours)})
             tmp_df = pd.DataFrame(stops, index=[0])
-            df = df.append(tmp_df, ignore_index=True)
+            result = result.append(tmp_df, ignore_index=True)
 
-  return df
+  return result
