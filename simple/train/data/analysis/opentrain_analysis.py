14c179cc4e59cf80f9f1dec811d5dd5f7ce15d4a
Ofer Bartal <greenofer@gmail.com>
2017-09-04 20:41:55 +0300

    Add excel output

--- simple/train/data/analysis/opentrain_analysis.py
+++ simple/train/data/analysis/opentrain_analysis.py
@@ -125,6 +125,6 @@ json_stats_table = stats_table[['route_id', 'year', 'month', 'week_day', 'hours'
 json_stats_table.to_json('routes_output_format_records.json', orient='records', lines=True)
 
 # TODO: Add "sudo pip install XlsxWriter" to installation
-writer = pd.ExcelWriter('output.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter('routes_output.xlsx', engine='xlsxwriter')
 stats_table.to_excel(writer, sheet_name='Sheet1')
 writer.save()
