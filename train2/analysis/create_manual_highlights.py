35007c0091b5b1773432f09a759e44fe91bdb852
Ofer Bartal <greenofer@gmail.com>
2017-09-11 21:37:41 +0300

    Add 3rd highlight

--- train2/analysis/create_manual_highlights.py
+++ train2/analysis/create_manual_highlights.py
@@ -19,12 +19,20 @@ url = "http://otrain.org/#/201707/routes/124?day=1"
 text = "2בדיקה בדיקה בדיקה"
 late_highlights.append(createHighlight(url, text))
 
-url = "http://otrain.org/#/201707/routes/124?time=15-18"
+url = "http://otrain.org/#/201707/routes/124?day=1"
 text = "3בדיקה בדיקה בדיקה"
+late_highlights.append(createHighlight(url, text))
+
+url = "http://otrain.org/#/201707/routes/124?time=15-18"
+text = "4בדיקה בדיקה בדיקה"
+ontime_highlights.append(createHighlight(url, text))
+
+url = "http://otrain.org/#/201707/routes/124?time=15-18"
+text = "5בדיקה בדיקה בדיקה"
 ontime_highlights.append(createHighlight(url, text))
 
 url = "http://otrain.org/#/201707/routes/124?day=1"
-text = "4בדיקה בדיקה בדיקה"
+text = "6בדיקה בדיקה בדיקה"
 ontime_highlights.append(createHighlight(url, text))
 
 with open('static/analysis/manual_highlights.json', 'w') as outfile:
