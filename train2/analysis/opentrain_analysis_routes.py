8de910b2c8000114a0379728acaf6b7c083cdef1
Ofer Bartal <greenofer@gmail.com>
2017-11-06 19:28:05 +0200

    Add back routes analysis code, add description (html geenrating) code

---
+++ train2/analysis/opentrain_analysis_routes.py
@@ -0,0 +1,146 @@
+"""Functions to help in analysis of website data using ipython notebook"""
+import collections
+import json
+import os
+import requests
+import pandas as pd
+
+DOMAIN = 'otrain.org'
+CACHE_PATH = 'cache/'
+URL_GUI = 'http://otrain.org/#/%d%02d-%d%02d/routes/%d?day=%s&time=%s'
+WEEK_DAYS = [1, 2, 3, 4, 5, 6, 7]
+HOURS = ['all',
+         [4, 7],
+         [7, 9],
+         [9, 12],
+         [12, 15],
+         [15, 18],
+         [18, 21],
+         [21, 24],
+         [24, 28],
+         ]
+
+
+def get_stops_ids_to_heb_name(all_stops):
+  """Return a map from stop id to Hebrew name"""
+  result = {}
+  for stop_info in all_stops:
+    result[stop_info['stop_id']] = stop_info['stop_short_name']
+  return result
+
+
+def get_routes_above_threshold(num_trips_threshold):
+  """Return a list of route ids with trip count above @num_trips_threshold"""
+  result = []
+  for route_id in range(0, 10000):
+    passed_filter = False
+    if os.path.isfile("%scache_timespan_%s.json" % (CACHE_PATH, route_id)):
+      with open("%scache_timespan_%s.json" % (CACHE_PATH, route_id), 'r') as input_file:
+        route_data = json.load(input_file)
+        for _, data in route_data:
+          for entry in data:
+            # Look at either days='all' or hours='all' and limit to minimal num_trip count
+            if ((entry['info']['week_day'] == 'all' or entry['info']['hours'] == 'all') and
+                entry['info']['num_trips'] > num_trips_threshold):
+              passed_filter = True
+    if passed_filter:
+      result.append(route_id)
+  return result
+
+
+def get_route_by_id(route_id, num_trips_threshold):
+  """Return route information by @route_id, filtered by @num_trips_threshold"""
+  result = pd.DataFrame()
+  with open("%scache_timespan_%s.json" % (CACHE_PATH, route_id), 'r') as input_file:
+    route_data = json.load(input_file)
+    for time, data in route_data:
+      for entry in data:
+        # Look at either days='all' or hours='all' and limit to minimal num_trip count
+        if ((entry['info']['week_day'] == 'all' or entry['info']['hours'] == 'all') and
+            entry['info']['num_trips'] > num_trips_threshold):
+          url_day = '' if entry['info']['week_day'] == 'all' else entry['info']['week_day']
+          url_hours = '' if entry['info']['hours'] == 'all' else '%d-%d' % \
+                  (entry['info']['hours'][0], entry['info']['hours'][1])
+          if entry['info']['hours'] in HOURS:
+            entry['info']['hours'] = HOURS.index(entry['info']['hours'])
+          else:
+            entry['info']['hours'] = -1
+          for stops in entry['stops']:
+            stops.update(entry['info'])
+            stops.update({'year': time[0], 'month': time[1]})
+            stops.update({'url':URL_GUI % (int(time[0]), int(time[1]),
+                                           int(route_id), url_day, url_hours)})
+            tmp_df = pd.DataFrame(stops, index=[0])
+            result = result.append(tmp_df, ignore_index=True)
+
+  return result
+
+
+def get_stats_table(stop_id_to_name):
+  """Returns a data table of routes with stats"""
+  table = pd.DataFrame()
+  for route_id in range(0, 10000):
+    print(route_id)
+    if os.path.isfile("%scache_timespan_%s.json" % (CACHE_PATH, route_id)):
+      with open("%scache_timespan_%s.json" % (CACHE_PATH, route_id), 'r') as input_file:
+        route_data = json.load(input_file)
+        for year_month, data in route_data:
+          for entry in data:          
+            keys = collections.OrderedDict()
+            keys["route_id"] = route_id
+            keys["year"] = year_month[0]
+            keys["month"] = year_month[1]
+            keys["week_day"] = entry["info"]["week_day"]
+            keys["hours"] = entry["info"]["hours"]
+            keys["num_trips"] = entry["info"]["num_trips"]
+            url_day = '' if entry['info']['week_day'] == 'all' else entry['info']['week_day']
+            url_hours = '' if keys["hours"] == 'all' else '%d-%d' % (keys["hours"][0], keys["hours"][1])            
+            keys["url"] = URL_GUI % (int(year_month[0]), int(year_month[1]), int(year_month[2]), int(year_month[3]), int(route_id), url_day, url_hours)            
+            if keys["num_trips"] == 0 or keys["num_trips"] < 30:
+              continue
+            keys["first_stop"] =  entry['stops'][0]['stop_id']
+            keys["last_stop"] = entry['stops'][-1]['stop_id']
+            keys["first_stop_hebrew"] = stop_id_to_name[entry['stops'][0]['stop_id']]
+            keys["last_stop_hebrew"] = stop_id_to_name[entry['stops'][-1]['stop_id']]
+            keys["last_stop_arrival_late_pct"] = entry['stops'][-1]['arrival_late_pct']
+            keys = pd.Series(keys)
+
+            stats = pd.Series()
+            stops = pd.DataFrame(entry["stops"])            
+            get_stat = lambda series, name: series.drop('stop_id').add_prefix(name + '_')
+            stats = stats.append(get_stat(stops.mean(), 'mean'))
+            stats = stats.append(get_stat(stops.median(), 'median'))
+            stats = stats.append(get_stat(stops.std(), 'std'))
+            stats = stats.append(get_stat(stops.min(), 'min'))
+            stats = stats.append(get_stat(stops.max(), 'max'))
+            
+            row = pd.Series()
+            row = row.append(keys)
+            row = row.append(stats)
+            if table.columns.size == 0:
+              table = pd.DataFrame(index=row.index.tolist())
+            table = pd.concat([table, row], axis=1)
+  table = table.transpose()
+  return table
+
+all_stops = json.loads(requests.get('http://{}/api/v1/stops'.format(DOMAIN)).text)
+stop_id_to_name = get_stops_ids_to_heb_name(all_stops)
+
+stats_table = get_stats_table(stop_id_to_name)
+print(stats_table.shape)
+print("")
+json_stats_table = stats_table[['route_id', 'year', 'month', 'week_day', 'hours', 'num_trips',
+    'url', 'first_stop', 'last_stop', 'last_stop_arrival_late_pct', 'mean_arrival_late_pct',
+    'max_arrival_late_pct']]
+
+json_stats_table.to_json('static/analysis/routes_output_format_records.json', orient='records', lines=True)
+
+excel_stats_table = stats_table[['year', 'month', 'week_day', 'hours', 'num_trips',
+    'url', 'first_stop_hebrew', 'last_stop_hebrew', 'last_stop_arrival_late_pct', 'mean_arrival_late_pct',
+    'max_arrival_late_pct']]
+percent_columns = ['last_stop_arrival_late_pct', 'mean_arrival_late_pct', 'max_arrival_late_pct']
+excel_stats_table.loc[:, percent_columns] *= 100
+# TODO: Add "sudo pip install XlsxWriter" to installation
+writer = pd.ExcelWriter('static/analysis/routes_output.xlsx', engine='xlsxwriter')
+excel_stats_table.to_excel(writer, sheet_name='Sheet1')
+writer.save()
---
+++ train2/analysis/opentrain_analysis_routes_description.py
@@ -0,0 +1,84 @@
+"""Functions to help in analysis of website data using ipython notebook"""
+import collections
+import json
+import os
+import requests
+
+SERVER_DOMAIN = 'otrain.org'
+LOCAL_DOMAIN = '127.0.0.1:8000'
+DOMAIN = SERVER_DOMAIN
+
+def get_stops():
+      """Return a list stops from the website API"""
+  all_stops = json.loads(requests.get('http://otrain.org/api/v1/stops').text)
+  result = {}
+  for stop_info in all_stops:
+    result.update({stop_info['stop_id']: [stop_info['heb_stop_names'][0], stop_info['stop_name']]})
+  return result
+
+def get_hebrew_description(origin, destination, highlight, stop_id_to_name):
+  month = "חודש "
+  heb_to = " ל"
+  everyday_heb = " כל יום "
+  aleph = "א"
+  bet = "ב"
+  gimel = "ג"
+  daled = "ד"
+  hey = "ה"
+  vav = "ו"
+  shabbat = "ש"
+  days = ["stub", aleph, bet, gimel, daled, hey, vav, shabbat]
+  between_hours = "בין השעות "
+  delays = " ממוצע איחורים:"
+  max_delay = " איחור מקסימאלי:"
+  last_stop_delay = " איחור בתחנה אחרונה:"
+  day_heb = "" if highlight["week_day"] == 'all' else days[highlight["week_day"]] + " "
+  hours_heb = "" if highlight["hours"] == 'all' else between_hours + str(highlight["hours"][0]) + "-" + str(highlight["hours"][1])
+  result = str(highlight["month"]) + "/" + str(highlight["year"]) + ": " + stop_id_to_name[origin] + heb_to + stop_id_to_name[destination]
+  result += "<br>" + everyday_heb + day_heb + hours_heb
+  result += "<br>" + delays + " {0:.0f}%".format(highlight['mean_arrival_late_pct'] * 100)
+  result += "<br>" + max_delay + " {0:.0f}%".format(highlight['max_arrival_late_pct'] * 100)
+  result += "<br>" + last_stop_delay + " {0:.0f}%".format(highlight['last_stop_arrival_late_pct'] * 100)
+  return result
+
+def get_stops_ids_to_heb_name(all_stops):
+  """Return a list stop ids from the website API"""
+  result = {}
+  for stop_info in all_stops:
+    result[stop_info['stop_id']] = stop_info['stop_short_name']
+  return result
+
+def save_highlights_html(data, stop_id_to_name, filepath):
+  count = 1
+  with open(filepath, 'w') as html_file:  
+    html_file.write('<!DOCTYPE html><html><body dir="rtl">')
+    html_file.write('<h1 dir="ltr">Highlights</h1>')
+    data = sorted(data, key=lambda highlight: highlight['mean_arrival_late_pct'], reverse=True)
+    for highlight in data:
+      html_file.write('<h3>{}</h3>'.format(count))
+      count += 1
+      url = highlight['url']
+      if url.endswith('time='):
+        url = url[:-5]
+      if url.endswith('day='):
+        url = url[:-4]
+      if url.endswith('&'):
+        url = url[:-1]
+      if url.endswith('time='):
+        url = url[:-5]
+      if url.endswith('day='):
+        url = url[:-4]
+      if url.endswith('?'):
+            url = url[:-1]
+      html_file.write('<span>{}</span><p>'.format(get_hebrew_description(highlight["first_stop"], highlight["last_stop"], highlight, stop_id_to_name)))
+      html_file.write('<a href="{}" target="_blank">{}</a><p>'.format(url, url))
+#{'num_trips': 44, 'week_day': 'all', 'url': 'http://otrain.org/#/201706/routes/2439?day=&time=', 'hours': 'all', 'mean_arrival_late_pct': 0.0669191919, 'route_id': 2439, 'month': 6, 'year': 2017}
+    html_file.write('</body></html>')
+
+
+all_stops = json.loads(requests.get('http://{}/api/v1/stops'.format(DOMAIN)).text)
+stop_id_to_name = get_stops_ids_to_heb_name(all_stops)
+path = "static/analysis/routes_output_format_records.json"
+with open(path) as fh:
+  data = [json.loads(line) for line in fh]
+save_highlights_html(data, stop_id_to_name, 'highlights.html')
