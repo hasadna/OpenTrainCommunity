fcab931a89395d529a8e3b19afd3abcbeda3cbf3
Ofer Bartal <greenofer@gmail.com>
2017-11-26 21:56:09 +0200

    Analysis fixes

--- train2/data/management/commands/global_benchmarks.py
+++ train2/data/management/commands/global_benchmarks.py
@@ -30,16 +30,6 @@ class Command(BaseCommand):
         for hour in range(24):
             self.compute_trips_per_hour(hour)
 
-    def compute_trips_per_day(self, day):
-        ok_qs = Trip.objects.filter(valid=True,date__gte=datetime.date(2017,5,1), date__week_day=day).annotate(c=Max('samples__delay_arrival'))
-        late_qs = ok_qs.filter(c__gte=300)
-        print('Trips with some late station for day = {} % = {}'.format(day, 100.0*late_qs.count()/ok_qs.count()))
-
-    def compute_trips_per_hour(self, hour):
-        ok_qs = Trip.objects.filter(valid=True,date__gte=datetime.date(2017,5,1), x_hour_local=hour).annotate(c=Max('samples__delay_arrival'))
-        late_qs = ok_qs.filter(c__gte=300)
-        print('Trips with some late station for hour = {} % = {}'.format(hour, 100.0*late_qs.count()/ok_qs.count()))
-
     def compute_samples_per_day(self, day):
         samples_ontime = Sample.objects.filter(delay_arrival__lt=5*60).filter(exp_arrival__week_day=day).filter(valid=True,exp_arrival__gte=datetime.date(2017,5,1)).count()
         samples_delayed = Sample.objects.filter(delay_arrival__gte=5*60).filter(exp_arrival__week_day=day).filter(valid=True,exp_arrival__gte=datetime.date(2017,5,1)).count()
@@ -52,3 +42,12 @@ class Command(BaseCommand):
         ontime_average = samples_ontime / (samples_ontime + samples_delayed)
         print("Samples for hour %d Ontime (less than 5 minutes delay): %.2f " % (hour, ontime_average))
 
+    def compute_trips_per_day(self, day):
+        ok_qs = Trip.objects.filter(valid=True,date__gte=datetime.date(2017,5,1), date__week_day=day).annotate(c=Max('samples__delay_arrival'))
+        late_qs = ok_qs.filter(c__gte=300)
+        print('Trips with some late station for day = {} % = {}'.format(day, 100-100.0*late_qs.count()/ok_qs.count()))
+
+    def compute_trips_per_hour(self, hour):
+        ok_qs = Trip.objects.filter(valid=True,date__gte=datetime.date(2017,5,1), x_hour_local=hour).annotate(c=Max('samples__delay_arrival'))
+        late_qs = ok_qs.filter(c__gte=300)
+        print('Trips with some late station for hour = {} % = {}'.format(hour, 100-100.0*late_qs.count()/ok_qs.count()))
