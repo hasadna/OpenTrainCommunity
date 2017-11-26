41a77601a28da754690480042cf440a2223cde4c
Ofer Bartal <greenofer@gmail.com>
2017-11-26 21:41:50 +0200

    Fixes

--- train2/data/management/commands/global_benchmarks.py
+++ train2/data/management/commands/global_benchmarks.py
@@ -19,13 +19,13 @@ LOGGER = logging.getLogger(__name__)
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for day in range(7):
-            self.compute_samples_per_day(day)
+            self.compute_samples_per_day(day+1)
 
         for hour in range(24):
             self.compute_samples_per_hour(hour)
 
         for day in range(7):
-            self.compute_trips_per_day(day)
+            self.compute_trips_per_day(day+1)
 
         for hour in range(24):
             self.compute_trips_per_hour(hour)
@@ -33,22 +33,22 @@ class Command(BaseCommand):
     def compute_trips_per_day(self, day):
         ok_qs = Trip.objects.filter(valid=True,date__gte=datetime.date(2017,5,1), date__week_day=day).annotate(c=Max('samples__delay_arrival'))
         late_qs = ok_qs.filter(c__gte=300)
-        print('Trips for day = {} % = {}'.format(day, 100.0*late_qs.count()/ok_qs.count()))
+        print('Trips with some late station for day = {} % = {}'.format(day, 100.0*late_qs.count()/ok_qs.count()))
 
     def compute_trips_per_hour(self, hour):
         ok_qs = Trip.objects.filter(valid=True,date__gte=datetime.date(2017,5,1), x_hour_local=hour).annotate(c=Max('samples__delay_arrival'))
         late_qs = ok_qs.filter(c__gte=300)
-        print('Trips for hour = {} % = {}'.format(hour, 100.0*late_qs.count()/ok_qs.count()))
+        print('Trips with some late station for hour = {} % = {}'.format(hour, 100.0*late_qs.count()/ok_qs.count()))
 
     def compute_samples_per_day(self, day):
         samples_ontime = Sample.objects.filter(delay_arrival__lt=5*60).filter(exp_arrival__week_day=day+1).filter(valid=True,exp_arrival__gte=datetime.date(2017,5,1)).count()
         samples_delayed = Sample.objects.filter(delay_arrival__gte=5*60).filter(exp_arrival__week_day=day+1).filter(valid=True,exp_arrival__gte=datetime.date(2017,5,1)).count()
         ontime_average = samples_ontime / (samples_ontime + samples_delayed)
-        print("Samples for day %d Ontime (less than 5 minutes delay): %.2f " % (day+1, ontime_average))
+        print("Samples for day %d Ontime (less than 5 minutes delay): %.2f " % (day, ontime_average))
 
     def compute_samples_per_hour(self, hour):
         samples_ontime = Sample.objects.filter(delay_arrival__lt=5*60).filter(exp_arrival__week_day=hour).filter(valid=True,exp_arrival__gte=datetime.date(2017,5,1)).count()
         samples_delayed = Sample.objects.filter(delay_arrival__gte=5*60).filter(exp_arrival__week_day=hour).filter(valid=True,exp_arrival__gte=datetime.date(2017,5,1)).count()
         ontime_average = samples_ontime / (samples_ontime + samples_delayed)
-        print("Samples for day %d Ontime (less than 5 minutes delay): %.2f " % (day+1, ontime_average))
+        print("Samples for day %d Ontime (less than 5 minutes delay): %.2f " % (day, ontime_average))
 
