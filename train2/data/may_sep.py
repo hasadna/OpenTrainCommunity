83e188de00d236bd2d53879f76a8ca38cb84c72c
Eran Keydar <ekeydar@gmail.com>
2017-12-11 15:24:26 +0200

    data

--- train2/data/may_sep.py
+++ train2/data/may_sep.py
@@ -11,12 +11,12 @@ def by_hour(hours):
             valid=True).filter(
             is_source=False,trip__date__gte=datetime.date(2017,5,1)).annotate(
             h=ExtractHour('exp_arrival')).filter(h__in=hours)
-    qs_ns_late = qs_ns.filter(exp_arrival__gte=300).count()
+    qs_ns_late = qs_ns.filter(delay_arrival__gte=300)
     qs_s = Sample.objects.filter(
             valid=True).filter(
-            is_source=False,trip__date__gte=datetime.date(2017,5,1)).annotate(
+            is_source=True,trip__date__gte=datetime.date(2017,5,1)).annotate(
             h=ExtractHour('exp_departure')).filter(h__in=hours)
-    qs_s_late = qs_s.filter(exp_departure__gte=300).count()
+    qs_s_late = qs_s.filter(delay_departure__gte=300)
     print('hours = %s' % (hours))
     print('non source = %d / %d => %.2f' % (qs_ns_late.count(), qs_ns.count(), 100 * qs_ns_late.count() / qs_ns.count()))
     print('source = %d / %d => %.2f' % (qs_s_late.count(), qs_s_late.count(), 100 * qs_s_late.count() / qs_s.count()))
