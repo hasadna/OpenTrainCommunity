0b92c39c4c182744fccdb4e0869a80a265357f89
Eran Keydar <ekeydar@gmail.com>
2018-07-17 08:00:09 +0300

    fix filters

--- train2/ui/app/src/js/RouteExplorer.js
+++ train2/ui/app/src/js/RouteExplorer.js
@@ -4,6 +4,8 @@ import 'angular-ui-bootstrap';
 import 'highcharts';
 import 'highcharts-ng';
 
+import {monthNames, daysTable} from './lib/dt_lib';
+
 import AppController from "./controllers/AppController";
 import SelectStopsController from "./controllers/SelectStopsController";
 import SelectRouteController from "./controllers/SelectRouteController";
@@ -127,59 +129,21 @@ app.config(['$routeProvider', 'env',
         });
     }]);
 
-let daysTable = {
-            0: 'ראשון',
-            1: 'שני',
-            2: 'שלישי',
-            3: 'רביעי',
-            4: 'חמישי',
-            5: 'שישי',
-            6: 'שבת',
-        };
-
-angular.module('RouteExplorer')
-    .filter('week_day1', function () {
-        return function (day) {
-            if (day == 'all') {
-                return 'כל הימים';
-            }
-            return daysTable[day-1] || `??? ${day}`;
+app.filter('week_day1', () => day => {
+        if (day == 'all') {
+            return 'כל הימים';
         }
-    }).filter('week_day0', function () {
-        return function (day) {
-            if (day == 'all') {
-                return 'כל הימים';
-            }
-            return daysTable[day] || `??? ${day}`;
+        return daysTable[day-1] ? daysTable[day-1].name : `??? ${day}`;
+    })
+    .filter('hours', () => hours => {
+        let fix = h => h >= 24 ? h % 24 : h;
+        if (hours == 'all') {
+            return 'כל היום'
         }
-    }).filter('hours', function () {
-        return function(hours) {
-            let fix = h => h >= 24 ? h % 24 : h;
-            if (hours == 'all') {
-                return 'כל היום'
-            }
-            let h1 = fix(hours[1]);
-            let h0 = fix(hours[0]);
-            return `${h1} - ${h0}`;
-        }
-    }).filter('month_name', function() {
-        let months = [
-          'ינואר',
-          'פברואר',
-          'מרץ',
-          'אפריל',
-          'מאי',
-          'יוני',
-          'יולי',
-          'אוגוסט',
-          'ספטמבר',
-          'אוקטובר',
-          'נובמבר',
-          'דצמבר'
-        ]
-        return function(m) {
-            return months[m-1];
-        }
-    });
+        let h1 = fix(hours[1]);
+        let h0 = fix(hours[0]);
+        return `${h1} - ${h0}`;
+    })
+    .filter('month_name', () => m => monthNames[m-1]);
 
 
