127c2cde8890b38ac4bc2eaf2f0cfc389a8d9df3
Eran Keydar <ekeydar@gmail.com>
2018-11-13 06:33:32 +0200

    add description

--- next-home/src/components/trips-charts.vue
+++ next-home/src/components/trips-charts.vue
@@ -1,9 +1,26 @@
 <template lang="html">
+    <div>
+    <div class="row">
+        <div class="col-12">
+            <div class="alert alert-info">
+                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                <p>
+            התרשימים מציגים את אחוז הרכבות המאחרות בחתך של חודשים. רכבת נחשבת מאחרת אם היא איחרה ב 5 דקות או יותר.
+                    </p>
+                <p>
+                ישנם שני מדדים לאיחור - הראשון הוא לפי האיחור המקסימלי לאורך המסלול, והשני הוא לפי האיחור בתחנה האחרונה (תחנת היעד)
+            </p>
+            </div>
+        </div>
+    </div>
     <div class="row">
         <div class="col-sm-6 col-12" :class="{'offset-sm-3': configs.length == 1 }"
              v-for="config in configs">
             <trips-chart :config="config"/>
         </div>
+    </div>
     </div>
 </template>
 
