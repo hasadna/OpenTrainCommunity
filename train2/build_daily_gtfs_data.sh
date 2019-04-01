410d28f5db36409c8237b1fd6ab7f55055db5962
Eran Keydar <ekeydar@gmail.com>
2019-04-01 21:08:14 +0300

    script

--- train2/build_daily_gtfs_data.sh
+++ train2/build_daily_gtfs_data.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
 source /home/opentrain/.virtualenvs/train2/bin/activate
 cd "$(dirname "${BASH_SOURCE[0]}")"
 python manage.py build_daily_gtfs_data
+
+# PUT THE BELOW IN /etc/cron.d/daily_gtfs
+
+# ###############################
+# # CRON TO DOWNLOAD GTFS
+# ###############################
+# 0 1 * * * opentrain /home/opentrain/OpenTrainCommunity/train2/build_daily_gtfs_data.sh >> /home/opentrain/cron.log 2>&1
 
