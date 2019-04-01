6218dfad25fcf8d74462103398081ca8a14d340a
Eran Keydar <ekeydar@gmail.com>
2019-04-01 21:13:22 +0300

    script

--- train2/common/management/commands/build_daily_gtfs_data.py
+++ train2/common/management/commands/build_daily_gtfs_data.py
@@ -2,7 +2,7 @@ import datetime
 import logging
 from django.core.management import BaseCommand
 from ...ot_gtfs_utils import get_or_create_daily_trips
-from ...slack_utils import send_message
+from ...slack_utils import send_error, send_info
 
 logger = logging.getLogger(__name__)
 
@@ -22,8 +22,8 @@ class Command(BaseCommand):
             logger.info("Started build_daily_gtfs_commands for date %s", date)
             daily = get_or_create_daily_trips(date, force=kwargs['force'])
             logger.info("End build_daily_gtfs_commands shape = %s", daily.shape)
-            send_message(f'build_daily_gtfs_commands ended successfully daily = {daily.shape}')
+            send_info(f'build_daily_gtfs_commands ended successfully daily = {daily.shape}')
         except Exception as ex:
             logger.exception(ex)
-            send_message(f'Failed in build_daily_gtfs_commands {ex}')
+            send_error(f'Failed in build_daily_gtfs_commands {ex}')
 
--- train2/common/slack_utils.py
+++ train2/common/slack_utils.py
@@ -7,12 +7,20 @@ from django.conf import settings
 logger = logging.getLogger(__name__)
 
 
-def send_message(msg):
+def send_error(msg):
+    send_message('ERROR', msg)
+
+
+def send_info(msg):
+    send_message('INFO', msg)
+
+
+def send_message(level, msg):
     url = settings.SLACK_URL
     payload = {
         'channel': '#opentrain',
         'username': 'webhookbot',
-        'text': f'[SYSTEM MESSAGE] {msg}',
+        'text': f'[{level}] {msg}',
         'icon_emoji': ':train2:'
     }
     return requests.post(url, data={'payload': json.dumps(payload)})
