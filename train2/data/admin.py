801c4e5ca063c720fe4878904d5dfb3ce79f3223
Eran Keydar <ekeydar@gmail.com>
2019-05-13 21:25:38 +0300

    stop admin

--- train2/data/admin.py
+++ train2/data/admin.py
@@ -7,8 +7,13 @@ from . import models
 
 @admin.register(models.Stop)
 class StopAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'english', 'hebrew_list_html', 'gtfs_stop_id', 'gtfs_code']
+    list_display = ['__str__', 'name_len', 'english', 'hebrew_list_html', 'gtfs_stop_id', 'gtfs_code']
     search_fields = ['hebrew_list', 'english']
+
+    def name_len(self, obj):
+        return len(str(obj))
+
+    name_len.short_description = 'name length'
 
     def hebrew_list_html(self, obj):
         return mark_safe('<br>'.join(escape(h) for h in obj.hebrew_list))
