51f1a588eb6eb55b5725d8d31db5de939df11619
Nir Gilboa <gilboa.nir@gmail.com>
2019-05-27 21:33:25 +0300

    Define ChatReport and add migration for it

--- train2/chatbot/models.py
+++ train2/chatbot/models.py
@@ -10,8 +10,21 @@ class ChatSession(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     current_step = models.CharField(max_length=50, default=INITIAL_STEP)
     payloads = ArrayField(models.TextField(null=True), default=list)
-    steps_data = JSONField(models.TextField(null=True), default=dict)
+    steps_data = JSONField(default=dict)
     last_save_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.user_id} started at @{self.created_at.replace(microsecond=0)}'
+
+
+class ChatReport(models.Model):
+    class ReportType:
+        CANCEL = 'cancel'
+        choices = [
+            (CANCEL, CANCEL)
+        ]
+    report_type = models.CharField(max_length=20, choices=ReportType.choices)
+    session = models.OneToOneField(ChatSession, related_name="report", on_delete=models.PROTECT)
+    full_trip = JSONField()
+    user_data = JSONField()
+
