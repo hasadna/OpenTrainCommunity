645bd530b1934f42e32524da547510e387ca9fb5
Nir Gilboa <gilboa.nir@gmail.com>
2019-06-03 17:49:39 +0300

    Save ChatReport to db before saying goodbye

--- train2/chatbot/steps/goodbye_step.py
+++ train2/chatbot/steps/goodbye_step.py
@@ -1,5 +1,9 @@
+import logging
+from chatbot import models
+from chatbot.chat_utils import ChatUtils
 from . import chat_step
 
+logger = logging.getLogger(__name__)
 
 class GoodbyeStep(chat_step.ChatStep):
     @staticmethod
@@ -7,8 +11,20 @@ class GoodbyeStep(chat_step.ChatStep):
         return 'goodbye'
 
     def send_message(self):
+        self.save_chat_report()
+
         message = 'קיבלתי 👍 תודה רבה על הדיווח, אני מקווה שתצליחו להגיע ליעד בקרוב... :)'
         self._send_message(message)
 
     def handle_user_response(self, messaging_event):
         return 'restart'
+
+    def save_chat_report(self):
+        reported_trip = ChatUtils.get_step_data('train_trip', self.session)
+        chat_report = models.ChatReport.objects.create(
+            report_type=models.ChatReport.ReportType.CANCEL,
+            session=self.session,
+            full_trip=reported_trip,
+            user_data=self.session.user_id)
+        logger.info("Created chat report %d", chat_report.id)
+
