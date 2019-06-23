b13a413d413993c5192aa56d014c1bc0cfe71e3c
Eran Keydar <ekeydar@gmail.com>
2019-06-23 18:04:06 +0300

    fix telegram scripts

--- train2/run_telegram_bot.sh
+++ train2/run_telegram_bot.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source /home/opentrain/.virtualenvs/train2/bin/activate
 cd $DIR
-python manage.py telegram_bot
+python manage.py run_telegram_bot
 
 
 
