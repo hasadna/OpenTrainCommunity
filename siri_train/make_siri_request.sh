3c78724c9f3ba1ba88fd6f80216390ca8fac109e
Eran Keydar <ekeydar@gmail.com>
2019-09-23 10:01:48 +0300

    siri train

--- siri_train/make_siri_request.sh
+++ siri_train/make_siri_request.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /home/opentrain/.virtualenvs/train2/bin/activate
 cd /home/opentrain/OpenTrainCommunity/siri_train
-python ./make_siri_train.py
+python ./make_siri_request.py
 
 
